767506c64f4e36bd1fb672bf0fdb1895f4d49cfb
weijwang18@gmail.com <weijwang18@gmail.com>
2022-07-29 14:50:24 -0700

    WIP:Updating Views

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,7 +1,10 @@
-namespace ProjectName.Models
+namespace HairSalon.Models
 {
-  public class ClassName
+  public class Client
   {
-    // properties, constructors, methods, etc. go here
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
   }
 }
